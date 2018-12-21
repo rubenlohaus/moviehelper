1e24e5d6473bccaa8d699cb0ab6df663064500b2
Ruben Lohaus <ruben@lohaus.cl>
2018-12-21 11:56:10 -0800

    First commit

---
+++ moviesearch.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+SEARCH=""
+ID=""
+TOKEN=""
+ROTTEN=""
+URL=""
+for i in "$@"
+do
+case $i in
+    -s=*|--search=*)
+    SEARCH="${i#*=}"
+    ;;
+    -i=*|--id=*)
+    ID="${i#*=}"
+    ;;		
+    -t=*|--token=*)
+    TOKEN="${i#*=}"
+    ;;
+    -r=*|--rotten=*)
+    ROTTEN="${i#*=}"
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+if [ -n "$SEARCH" ]; then
+    if [ -n "$TOKEN" ]; then
+        URL="http://www.omdbapi.com/?s=$SEARCH&apikey=$TOKEN"
+        curl $URL | python -m json.tool
+    else 
+        echo "Token is mandatory"
+    fi
+else
+    if [ -n "$TOKEN" ]; then
+        URL="http://www.omdbapi.com/?i=$ID&tomatoes=$ROTTEN&apikey=$TOKEN"
+        curl $URL | python -m json.tool
+    else 
+        echo "Token is mandatory"
+    fi
+fi
